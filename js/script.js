6929df66de13544c5ff17921490f5c502871ea61
Salim Sezer Kurnaz <salimsezer.dev@gmail.com>
2022-12-15 22:40:20 +0300

    saat eklendi 2. menü eklendi

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-const saat = new Date();
-document.getElementById("minute").innerHTML = saat.getMinutes();
+// saat
+window.onload = startTime;
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+
+  h = checkTime(h);
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("saat").innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout("startTime()", 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+// saat
